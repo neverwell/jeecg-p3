59d79be8bf00b26e8c1ef91537f42027ad071246
neverwell <292519287@qq.com>
2016-11-01 15:16:31 +0800

    产品管理查询修复

--- jeecg-p3-biz-cms/src/main/java/com/jeecg/cms/sql/ErpCodeDao_condition.sql
+++ jeecg-p3-biz-cms/src/main/java/com/jeecg/cms/sql/ErpCodeDao_condition.sql
@@ -1,8 +1,8 @@
-<#if (erpCode.code)??>
+<#if (erpCode.code)?? && erpCode.code ?length gt 0>
 		    /* CODE */
 			and ec.CODE = :erpCode.code
 </#if>
-<#if (erpCode.oneCode)??>
+<#if (erpCode.oneCode)?? && erpCode.oneCode ?length gt 0>
 		    /* ONECODE */
 			and ec.ONECODE = :erpCode.oneCode
 </#if>
--- jeecg-p3-biz-cms/src/main/java/com/jeecg/cms/sql/ErpProductDao_condition.sql
+++ jeecg-p3-biz-cms/src/main/java/com/jeecg/cms/sql/ErpProductDao_condition.sql
@@ -1,4 +1,20 @@
-		<#if (erpProduct.columnId)?? && erpProduct.columnId ?length gt 0>
+<#if (erpProduct.columnId)?? && erpProduct.columnId ?length gt 0>
 		    /* 类型id */
 			and ep.column_id like CONCAT('%', :erpProduct.columnId ,'%') 
-		</#if>
+</#if>
+<#if (erpProduct.name)?? && erpProduct.name ?length gt 0>
+		    /* ONECODE */
+			and ep.NAME = :erpProduct.name
+</#if>
+<#if (erpProduct.location)?? && erpProduct.location ?length gt 0>
+		    /* ONECODE */
+			and ep.LOCATION = :erpProduct.location
+</#if>
+<#if (erpProduct.brand)?? && erpProduct.brand ?length gt 0>
+		    /* ONECODE */
+			and ep.BRAND = :erpProduct.brand
+</#if>
+<#if (erpProduct.model)?? && erpProduct.model ?length gt 0>
+		    /* ONECODE */
+			and ep.MODEL = :erpProduct.model
+</#if>
